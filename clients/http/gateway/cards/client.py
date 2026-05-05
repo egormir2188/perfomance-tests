3ce4d175435a18676f8807dc6d1c6540d0a41a5d
egormir2188 <yaegormir2188@gmail.com>
2026-05-05 23:49:00 +0400

    Написание HTTP API клиента

---
+++ clients/http/gateway/cards/client.py
@@ -0,0 +1,30 @@
+from httpx import Response
+from typing import TypedDict
+
+from clients.http.client import HttpClient
+
+
+class IssueCardRequest(TypedDict):
+    userId: str
+    accountId: str
+
+class CardsGatewayClient(HttpClient):
+    """
+    Клиент для взаимодействия с /api/v1/cards/ сервиса http-gateway.
+    """
+
+    def issue_virtual_card_api(self, request: IssueCardRequest) -> Response:
+        """
+        Метод для выпуска виртуальной карты.
+        :param request: Запрос на создание виртуальной карты (объект IssueCardRequest).
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.post('/api/v1/cards/issue-virtual-card', json=request)
+
+    def issue_physical_card_api(self, request: IssueCardRequest) -> Response:
+        """
+        Метод для выпуска физической карты.
+        :param request: Запрос на создание физической карты (объект IssueCardRequest).
+        :return: Ответ от сервера (объект httpx.Response).
+        """
+        return self.post('/api/v1/cards/issue-physical-card', json=request)
